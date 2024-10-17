8aa52ba9712c739492db95f449ec7d5f8152b232
anijit18 <121957827+anijit18@users.noreply.github.com>
2024-10-17 20:27:22 +0530

    Create Remove-All-Duplicates-From-The-Given-String.cpp

---
+++ March 2024/Remove-All-Duplicates-From-The-Given-String.cpp
@@ -0,0 +1,22 @@
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+     string ans="";
+     unordered_map<char,int>mp;
+     for(int i: str)
+        mp[i]++;
+     for(int i=str.size();i>=0;i--)
+     {
+         if(mp[str[i]]>1)
+         {
+             mp[str[i]]--;
+         }
+         else
+         ans+=str[i];
+     }
+     reverse(ans.begin(),ans.end());
+     return ans;
+    
+     }
+};
